7015e7ce620f04b61f346f2dd6ba5c64ffa15754
dejan-rankic <dejan.rankic@student.um.si>
2026-05-07 23:34:25 +0200

    SCRUM-70 Configure ARSO service URLs

--- EnviroWatchSI/server/src/main/kotlin/com/envirowatchsi/network/MeteoFetcher.kt
+++ EnviroWatchSI/server/src/main/kotlin/com/envirowatchsi/network/MeteoFetcher.kt
@@ -3,6 +3,8 @@ package com.envirowatchsi.network
 import java.net.URL
 
 fun fetchRawMeteoXml(): String {
-    val url = URL("https://meteo.arso.gov.si/uploads/probase/www/observ/surface/text/sl/observation_si_latest.xml")
-    return url.openStream().bufferedReader(Charsets.UTF_8).readText()
+    return URL(ArsoConfig.METEO_URL)
+        .openStream()
+        .bufferedReader(Charsets.UTF_8)
+        .readText()
 }
