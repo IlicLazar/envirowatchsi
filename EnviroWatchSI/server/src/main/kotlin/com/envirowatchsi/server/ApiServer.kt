0f4a93811a4ecbf14b4c7fc0f20782da6a9109cd
dejan-rankic <dejan.rankic@student.um.si>
2026-05-08 11:28:50 +0200

    SCRUM-138 Add API connection test

--- EnviroWatchSI/server/src/main/kotlin/com/envirowatchsi/server/ApiServer.kt
+++ EnviroWatchSI/server/src/main/kotlin/com/envirowatchsi/server/ApiServer.kt
@@ -5,6 +5,7 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import org.jetbrains.exposed.sql.transactions.transaction
 
 object ApiServer {
 
@@ -24,9 +25,20 @@ fun Application.module() {
         get("/") {
             call.respondText("EnviroWatch SI API is running")
         }
-
         get("/health") {
             call.respondText("OK")
+        }
+        get("/api/status") {
+            val databaseStatus = try {
+                transaction {
+                    exec("SELECT 1")
+                }
+                "connected"
+            } catch (e: Exception) {
+                "error: ${e.message}"
+            }
+
+            call.respondText("API running, database: $databaseStatus")
         }
     }
 }
