d3642b757f1da6b14bb1f1fba1c0698518703cac
dejan-rankic <dejan.rankic@student.um.si>
2026-05-16 02:14:56 +0200

    SCRUM-269 Add Swagger API documentation for CRUD routes

--- envirowatchsi-api/src/routes/airQualityRoutes.js
+++ envirowatchsi-api/src/routes/airQualityRoutes.js
@@ -8,9 +8,66 @@ const {
   deleteAirQuality,
 } = require("../controllers/airQualityController");
 
+/**
+ * @swagger
+ * /api/air-quality:
+ *   get:
+ *     summary: Get all air quality records
+ *     tags: [Air Quality]
+ *     responses:
+ *       200:
+ *         description: List of air quality records
+ */
 router.get("/", getAllAirQuality);
+
+/**
+ * @swagger
+ * /api/air-quality:
+ *   post:
+ *     summary: Create a new air quality record
+ *     tags: [Air Quality]
+ *     requestBody:
+ *       required: true
+ *     responses:
+ *       201:
+ *         description: Air quality record created
+ */
 router.post("/", createAirQuality);
+
+/**
+ * @swagger
+ * /api/air-quality/{id}:
+ *   put:
+ *     summary: Update an air quality record
+ *     tags: [Air Quality]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Air quality record updated
+ */
 router.put("/:id", updateAirQuality);
+
+/**
+ * @swagger
+ * /api/air-quality/{id}:
+ *   delete:
+ *     summary: Delete an air quality record
+ *     tags: [Air Quality]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Air quality record deleted
+ */
 router.delete("/:id", deleteAirQuality);
 
 module.exports = router;
