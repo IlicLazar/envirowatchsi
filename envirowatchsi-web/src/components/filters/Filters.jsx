36d2a1e92589de9bc9e643ddc7e34ccca5827152
Lazar Ilic <iliclazar16@gmail.com>
2026-05-17 15:17:01 +0200

    SCRUM-224 Added 'Reset filter' button that clears all 5 current filter fields

--- envirowatchsi-web/src/components/filters/Filters.jsx
+++ envirowatchsi-web/src/components/filters/Filters.jsx
@@ -70,6 +70,20 @@ function Filters({ filters, onFilterChange }) {
           />
         </div>
       </div>
+      
+      <button
+        onClick={() => onFilterChange({})}
+        style={{
+          padding: "8px 16px",
+          backgroundColor: "#f44336",
+          color: "white",
+          border: "none",
+          borderRadius: "4px",
+          cursor: "pointer",
+        }}
+      >
+        Reset Filters
+      </button>
     </div>
   );
 }
