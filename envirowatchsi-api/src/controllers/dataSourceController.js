a3a537afb9f8fe6383707ed39445a98a7ea536e3
NikolinaLukic <nikolina.lukic@student.um.si>
2026-05-18 09:42:17 +0200

    SCRUM-242 Add PUT endpoint for data sources

--- envirowatchsi-api/src/controllers/dataSourceController.js
+++ envirowatchsi-api/src/controllers/dataSourceController.js
@@ -41,3 +41,29 @@ exports.createDataSource = async (req, res) => {
     });
   }
 };
+
+exports.updateDataSource = async (req, res) => {
+  try {
+    const updatedDataSource = await DataSource.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    if (!updatedDataSource) {
+      return res.status(404).json({
+        message: "Data source not found",
+      });
+    }
+
+    res.status(200).json(updatedDataSource);
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to update data source",
+      error: error.message,
+    });
+  }
+};
